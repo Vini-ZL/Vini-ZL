d1e6746e1ad991197fbe2c4db9c0d17c8d596e29
VINICIUS GONCALVES MESQUITA <vinicius.goncalves2503@gmail.com>
2025-08-30 22:13:44 -0300

    Create user_model.dart

---
+++ banco_costa/lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class User {
+  final String cpfCnpj;
+  final String password;
+  final String fullName;
+
+  User({
+    required this.cpfCnpj,
+    required this.password,
+    required this.fullName,
+  });
+}
